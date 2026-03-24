dcdd47e7130f83cb48eefe8ea6fd246d2c3fc95c
zammu026 <samueldelao2026@gmail.com>
2026-03-24 14:10:15 -0600

    update

--- parcial1_codes/series.py
+++ parcial1_codes/series.py
@@ -2,6 +2,6 @@ from sympy import symbols, Sum, oo
 n = symbols('n')
 # Ejemplo: Suma de 1/n desde 1 hasta k
 k = symbols('k')
-termino_general = 1/n**2
+termino_general = 1/n # Puedes cambiar esto por cualquier término general que desees
 suma_general = Sum(termino_general, (n, 1, k))
 print(suma_general.subs(k, 10).doit().evalf()) 
---
+++ semana7/modelo1_RK4.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# parametros
+k = 1.0
+m = 1.0
+omega_0 = np.sqrt(k/m)
+
+mu = 0.001 #friccion viscosa
+F_drive = 1.0
+t_max = 100
+N = 5000
+t_values = np.linspace(0, t_max, N)
+h = t_values[1] - t_values[0]
+
+# condiciones iniciales
+x0 = 0.0
+v0 = 0.0
+
+def acceleration(t, x, v, alpha, omega_f):
+    return -k*x/m + k*alpha*(x**2)/m - mu*v/m + F_drive*np.cos(omega_f*t)/m
+
+def rk4(alpha, omega_f):
+    x = np.zeros(N)
+    v = np.zeros(N)
+    x[0] = 0.0
+    v[0] = 0.0
+    for i in range(N-1):
+        t = t_values[i]
+        k1_x = v[i]
+        k1_v = acceleration(t, x[i], v[i], alpha, omega_f)
+        k2_x = v[i] + 0.5*h*k1_v
+        k2_v = acceleration(t + 0.5*h, x[i] + 0.5*h*k1_x, v[i] + 0.5*h*k1_v, alpha, omega_f)
+        k3_x = v[i] + 0.5*h*k2_v
+        k3_v = acceleration(t + 0.5*h, x[i] + 0.5*h*k2_x, v[i] + 0.5*h*k2_v, alpha, omega_f)
+        k4_x = v[i] + h*k3_v
+        k4_v = acceleration(t + h, x[i] + h*k3_x, v[i] + h*k3_v, alpha, omega_f)
+        x[i+1] = x[i] + (h/6.0)*(k1_x + 2*k2_x + 2*k3_x + k4_x)
+        v[i+1] = v[i] + (h/6.0)*(k1_v + 2*k2_v + 2*k3_v + k4_v)
+    return x, v
+
+# Graficar
+omega_f = 2.0 # frecuencia de la fuerza externa oscilatoria
+F_drive = 5.0 # amplitud de la fuerza de externa
+x_ml, v_ml = rk4(alpha=0.10, omega_f=omega_f)
+plt.plot(t_values, x_ml)
+plt.xlabel("Tiempo (s)")
+plt.ylabel("Posición (m)")
+plt.title("Oscilador Armónico No Lineal Amortiguada Forzada con RK4, mode Locking wf = 2, fuerza grande")
+plt.grid()
+plt.show()
+
+plt.plot(x_ml, v_ml)
+plt.xlabel("Posición (m)")
+plt.ylabel("Velocidad (m/s)")
+plt.title("Oscilador Armónico No Lineal Amortiguada Forzada con RK4, mode Locking wf = 2, fuerza grande")
+plt.grid()
+plt.show()
