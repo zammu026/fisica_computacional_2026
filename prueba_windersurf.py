dc6196282ecf870dd2bc192366ddce176f3764ef
zammu026 <samueldelao2026@gmail.com>
2026-04-10 00:47:18 -0600

    update

--- prueba_windersurf.py
+++
@@ -1,3 +0,0 @@
-def ley_wiens(nu, T):
-    return 2.897772e-3 * nu**2 / T  # mK
-print (ley_wiens(1, 300))
---
+++ semana8/fuerza_perturbacion.py
@@ -0,0 +1,3 @@
+# sistema vec_F = (-k/r^6 + C/r^3) r_unitario
+# casos: |C| < l^2/m, |C| = l^2/m, |C| > l^2/m
+# Resolver numericamente, identificar estados ligandos y analizar precesion
