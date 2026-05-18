2a224eb24c6e1d6125ad63665fdfe30a0c360288
zammu026 <samueldelao2026@gmail.com>
2026-05-18 16:27:20 -0600

    update

---
+++ semana13/Problema2_viernes_metodo_relajacion.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from scipy 
+
+# Parametros
+epsilon0 = 8.854e-12
+
+a = 20.0
+L = 20.0
+
+# parametros numericos
+Nr = 80
+Nz = 80
+
+dr = a/(Nr - 1)
+dz = L/(Nz - 1)
+
+rho = np.linspace(0, a, Nr)
+z = np.linspace(0, L, Nz)
+
+# falta
+# Densidad de carga
+rho_charge = np.zeros((Nz, Nr))
+
+# Carga puntual aproximada
+q = 1e-9
+
+rho0 = 8.0
+z0 = 10.0
+
+ir = int(rho0/dr)
+iz = int(z0, dz)
+
+# Aproximacion discreta de delta de dirac
+rho_charge[iz, dr] = #falta
+
+# parametros iterativos
+
+
+
+
+Nrho = 60
+Nphi = 80
+
+# funcion potencial
+def potencial(rho, phi, z):
+    suma_total = np.zeros_like(rho_dtype =)
